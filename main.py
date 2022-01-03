262ba9ad8e587c7e575c58ae56a8739ec1c50b98
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-01-03 14:28:58 +0000

    radom commit #

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ from discord import Webhook, RequestsWebhookAdapter
 PATH_OF_GIT_REPO = r'.git'  # make sure .git folder is properly configured
 today = datetime.datetime.now()
 today = today.strftime('%j')
+print(f"today -> {today}")
+today = 1
 webhook = Webhook.from_url("https://discord.com/api/webhooks/926421181870006303/X98DdFguI7lkpQKUhrkp5tjx-uYfP1P5e_5doL8AWhbdPvXcfmOLiboqsWSeMAO3Cf8f" ,adapter=RequestsWebhookAdapter())
-file = open("./ori.push.py", "rw")
+file = open("./ori.push.py", "r+")
 f = file.read()
 file.write(f[today:])
 file.close()
@@ -26,5 +28,5 @@ def git_push(commit, chr):
 	    webhook.send("@Admin error bande de troll")
 
 for i in first:
-	git_push()
+	git_push(f"radom commit {i}", i)
 	sleep(3*60)
