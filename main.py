32c58918f39a815d03679413c7779520c33e8755
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-01-19 22:21:54 +0000

    day : 19, commit : 01/19 -> `e`

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ from discord import Webhook, RequestsWebhookAdapter
 PATH_OF_GIT_REPO = r'.git'  # make sure .git folder is properly configured
 today = datetime.datetime.now()
 today = int(today.strftime('%j'))
-webhook = Webhook.from_url("https://discord.com/api/webhooks/932789158953484338/GHgyHUxKAiRVnsVgckj8SAwgvKs5tuDzuEXQXToWOSZeHOtT1OsdyrO0cl75DiJ3o25f", adapter=RequestsWebhookAdapter())
-webhook.send(f"Salut on commence a push 😊")
+#webhook = Webhook.from_url("https://discord.com/api/webhooks/932789158953484338/GHgyHUxKAiRVnsVgckj8SAwgvKs5tuDzuEXQXToWOSZeHOtT1OsdyrO0cl75DiJ3o25f", adapter=RequestsWebhookAdapter())
+#webhook.send(f"Salut on commence a push 😊")
 
 f = open("./ori.push.py", "r").read()
 first = f[:today]
 open("./ori.push.py", "w").write(f[today:])
 
-print(f"day -> {today} -> '{first.encode('unicode_escape').decode()}'")
-webhook.send(f"start commit : day -> {today} -> `{first.encode('unicode_escape').decode()}`")
+#print(f"day -> {today} -> '{first.encode('unicode_escape').decode()}'")
+#webhook.send(f"start commit : day -> {today} -> `{first.encode('unicode_escape').decode()}`")
 
 def git_push(commit, chr):
     print(commit)
@@ -30,9 +30,11 @@ def git_push(commit, chr):
         repo.index.commit(commit)
         origin = repo.remote(name='origin')
         origin.push()
-        webhook.send(commit)
-    except:
-        webhook.send("@Admin error bande de troll")
+        #webhook.send(commit)
+    except Exception as e:
+        print(f"date -> {datetime.datetime.now()}", file=sys.stderr)
+        print(f"{e}", file=sys.stderr)
+        #webhook.send("@Admin error bande de troll")
 
 for ind,i in enumerate(first):
     git_push(f"day : {today}, commit : {str(ind + 1).zfill(len(str(today)))}/{today} -> `{i.encode('unicode_escape').decode()}`", i)
