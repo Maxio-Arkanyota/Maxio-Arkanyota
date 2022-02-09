d8f5720ce72de18ca3b9bc220a77c25701591062
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-02-09 00:13:12 +0000

    day : 40, commit : 17/40 -> `_`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -14,4 +14,4 @@ types 		= globals()['__builtins__'].__dict__['__import__']("types")
 time 		= globals()['__builtins__'].__dict__['__import__']("time")
 sys 		= globals()['__builtins__'].__dict__['__import__']("sys")
 re 			= globals()['__builtins__'].__dict__['__import__']("re")
-os 			= globals()['__builtins_
+os 			= globals()['__builtins__
