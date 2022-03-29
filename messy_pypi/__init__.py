21930634eb88692039c042117e91350dcc643773
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-03-29 00:37:37 +0000

    day : 88, commit : 65/88 -> ` `

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -118,4 +118,4 @@ class Nonblocking(object):
 		self.stream = stream
 		self.fd = self.stream.fileno()
 	def __enter__(self):
-		self.orig_fl = fcntl.fcntl(self.fd,
+		self.orig_fl = fcntl.fcntl(self.fd, 
