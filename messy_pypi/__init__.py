57f357cb8624e9b95d21c33601838cfe5dffc22a
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-03-31 00:18:17 +0000

    day : 90, commit : 27/90 -> `l`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -121,4 +121,4 @@ class Nonblocking(object):
 		self.orig_fl = fcntl.fcntl(self.fd, fcntl.F_GETFL)
 		fcntl.fcntl(self.fd, fcntl.F_SETFL, self.orig_fl | os.O_NONBLOCK)
 	def __exit__(self, *args):
-		fcntl.fcntl(self.fd, fcnt
+		fcntl.fcntl(self.fd, fcntl
