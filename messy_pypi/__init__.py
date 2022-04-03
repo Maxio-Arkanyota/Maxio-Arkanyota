44dd947466214c6831e97e436c69c15a9ce6054f
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-04-03 00:33:31 +0000

    day : 93, commit : 57/93 -> `o`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -134,4 +134,4 @@ def clear() -> None:
 		os.system("cls")
 	else:
 		os.system("clear")
-def terminal_size(item: str = None) -> (tuple[int, int] 
+def terminal_size(item: str = None) -> (tuple[int, int] o
