23bd692b5a0559a0667711cea176b3e1984f0983
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-04-09 00:11:09 +0000

    day : 99, commit : 13/99 -> `:`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -157,4 +157,4 @@ def message_page_trop_petite(sizex, sizey) -> bool:
 	return False
 def terminal_dico(table, name_bar=("name", "ligne")):
 	len_of_each_elements = [0]*len(name_bar)
-	for i in range(len(len_of_each_elements))
+	for i in range(len(len_of_each_elements)):
