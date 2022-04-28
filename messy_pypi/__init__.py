f0754e621581902abb8db42de142eb49a6f1fc15
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-04-28 00:58:26 +0000

    day : 118, commit : 106/118 -> ` `

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -253,4 +253,4 @@ def getKey(debug: bool = False) -> str:
 			return n, c
 		def getkey():
 			n, c = getch()
-			# 0xE0 is 'grey' keys.  change this if you don't like it, but I don't care what color the
+			# 0xE0 is 'grey' keys.  change this if you don't like it, but I don't care what color the 
