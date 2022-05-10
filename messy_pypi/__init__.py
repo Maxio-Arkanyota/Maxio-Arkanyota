2bc5b1a60a8484a0e82954af100b6c04652dad56
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-05-10 01:07:06 +0000

    day : 130, commit : 123/130 -> `t`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -306,4 +306,4 @@ def get_key_bytes(debug: bool = False) -> bytes:
 # 	compressint -x [file_output]
 # 	Marche avec un input
 # Deompression:
-# 	compressint -X <file_compresser> [file_outpu
+# 	compressint -X <file_compresser> [file_output
