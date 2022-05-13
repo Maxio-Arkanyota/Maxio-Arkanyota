d93739e23264f3cbc9f07e2244e5049070acc9fc
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-05-13 00:41:10 +0000

    day : 133, commit : 072/133 -> `a`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -319,4 +319,4 @@ class CompressInt:
 	@staticmethod
 	def owrite(stringNbRandom, outfile):
 		"""
-		Entree: stringNbRandom: str, outfile: str(file n
+		Entree: stringNbRandom: str, outfile: str(file na
