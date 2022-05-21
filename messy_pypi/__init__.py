ea5d5170b421265d8fb89b1949047db7618e5af7
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-05-21 00:14:42 +0000

    day : 141, commit : 020/141 -> `e`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -365,4 +365,4 @@ class CompressInt:
 			if stringNombre.isdigit():
 				self.owrite(stringNombre, file_output)
 			else:
-				raise Exception("Un nombre est att
+				raise Exception("Un nombre est atte
