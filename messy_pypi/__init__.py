75cb60eec13c9bb3ef57091442bf94e1b4c31527
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-05-29 00:40:08 +0000

    day : 149, commit : 070/149 -> `e`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -416,4 +416,4 @@ def demineur():
 	while game_open:
 		if player_a_gagner:
 			clear()
-			print_char(1, 1, "Vous avez gagné\n" + "restart: Enter\n" + "
+			print_char(1, 1, "Vous avez gagné\n" + "restart: Enter\n" + "e
