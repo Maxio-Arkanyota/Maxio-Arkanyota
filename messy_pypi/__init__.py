acb64da62a0f0417dc1191c01a9fc2ddae011a79
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-05-30 00:48:18 +0000

    day : 150, commit : 086/150 -> `x`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -422,4 +422,4 @@ def demineur():
 				demineur()
 		elif ligne:
 			clear()
-			print_char(1, 1, "Vous avez perdu\n" +"restart: Enter\n" + "e
+			print_char(1, 1, "Vous avez perdu\n" +"restart: Enter\n" + "ex
