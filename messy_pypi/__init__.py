e702418ecff605f387ce92bad37162b22c16dd37
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-06-03 00:56:29 +0000

    day : 154, commit : 102/154 -> `,`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -442,4 +442,4 @@ def demineur():
 						char_item = "?"
 					print_char(
 						((terminal_size("X") - size) // 2) + i,
-						((terminal_size("Y") - size) // 2) + j
+						((terminal_size("Y") - size) // 2) + j,
