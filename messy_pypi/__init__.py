4a413fda6e8251bc000016cc0f3268fe3d5e6cf3
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-06-11 02:13:57 +0000

    day : 162, commit : 128/162 -> `e`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -487,4 +487,4 @@ def demineur():
 							 "⚑" if elem  # ⚐⚑
 							 else char_item_lambda)(drapeau_map[i][j], char_item)
 						)
-				print_char(((terminal_size("X") - siz
+				print_char(((terminal_size("X") - size
