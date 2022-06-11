3c90108a54ef9fcc3d30b5488f85892985f7990a
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-06-11 01:44:29 +0000

    day : 162, commit : 072/162 -> `_`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -485,4 +485,4 @@ def demineur():
 							((terminal_size("Y") - size) // 2) + j,
 							(lambda elem, char_item_lambda:
 							 "⚑" if elem  # ⚐⚑
-							 else char_item_lambda)(drapeau_map[i][j], char
+							 else char_item_lambda)(drapeau_map[i][j], char_
