1e946df195b55628c1ad9c22b6b867dc7bbf1533
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-06-19 00:57:45 +0000

    day : 170, commit : 101/170 -> `M`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -527,4 +527,4 @@ class MineSweeperMain:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					running = False
-				if event.type == pygame.MOUSEBUTTONDOWN and state == 
+				if event.type == pygame.MOUSEBUTTONDOWN and state == M
