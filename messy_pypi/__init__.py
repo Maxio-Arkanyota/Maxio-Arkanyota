93a0348bf91368be44acc8da8e67dc6f03e22b3b
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-06-20 00:53:33 +0000

    day : 171, commit : 093/171 -> `r`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -531,4 +531,4 @@ class MineSweeperMain:
 					if pygame.mouse.get_pressed()[0]:
 						pos = pygame.mouse.get_pos()
 						grid.click(pos[0], pos[1])
-					elif pygame.mouse.get_p
+					elif pygame.mouse.get_pr
