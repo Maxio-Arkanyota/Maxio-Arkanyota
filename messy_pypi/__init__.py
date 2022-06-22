10cd47f311ea072d61a6e10917af46fd5635bed3
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-06-22 00:25:01 +0000

    day : 173, commit : 039/173 -> `M`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -537,4 +537,4 @@ class MineSweeperMain:
 					if grid.check_if_win():
 						state = MineSweeperStates.win
 				if event.type == pygame.KEYDOWN:
-					if event.key == pygame.K_SPACE and (state == MineSweeperStates.game_over or state == 
+					if event.key == pygame.K_SPACE and (state == MineSweeperStates.game_over or state == M
