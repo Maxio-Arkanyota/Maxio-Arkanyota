096f96ef0036fff674fb0bbce9c8c8423fd97926
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-06-24 00:13:58 +0000

    day : 175, commit : 018/175 -> `v`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -546,4 +546,4 @@ class MineSweeperMain:
 			if player.get_health() == 0:
 				state = MineSweeperStates.game_over
 			if state == MineSweeperStates.game_over:
-				MineSweeperStats.draw(surface, 'Game o
+				MineSweeperStats.draw(surface, 'Game ov
