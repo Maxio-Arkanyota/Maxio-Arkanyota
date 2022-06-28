267b6b26d3fbda1d085827b077fce6e6460c72e6
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-06-28 01:06:04 +0000

    day : 179, commit : 117/179 -> `,`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -569,4 +569,4 @@ class MineSweeperPlayer:
 class MineSweeperStats:
 	@staticmethod
 	def draw(surface, label, pos):
-		textsurface = pygame.font.SysFont('Comic Sans MS'
+		textsurface = pygame.font.SysFont('Comic Sans MS',
