17c56934191ef0c02e48a2477d918d60f3c3e17e
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-07-01 00:52:23 +0000

    day : 182, commit : 091/182 -> `a`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -585,4 +585,4 @@ class MineSweeperCell:
 		self.marked = False
 		self.explosion = False
 		self.img_flag = pygame.image.load('./resources/minesweeper/cell-flagged.png')
-		self.img_flag = pyg
+		self.img_flag = pyga
