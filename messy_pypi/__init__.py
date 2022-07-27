53d8d2421602384d41bb86d48ed9e8804936e7ca
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-07-27 00:58:14 +0000

    day : 208, commit : 102/208 -> `n`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -731,4 +731,4 @@ class SnakeActions:
 			"\x1b[C": cls.change_direction,
 			"\x1b[D": cls.change_direction,
 			"m": SnakeDraw.show_menu,
-			"escape": SnakeDraw.show_me
+			"escape": SnakeDraw.show_men
