f7aca271643f4ab6405c25498f62a415bf154e75
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-07-28 00:52:58 +0000

    day : 209, commit : 092/209 -> `-`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -737,4 +737,4 @@ class SnakeActions:
 	@classmethod
 	def change_option_dead(cls, **kwargs):
 		if kwargs["clean_key"] in ["z", "\x1b[A"]:
-			SnakeDraw.dead_option_number = (SnakeDraw.dead_option_number 
+			SnakeDraw.dead_option_number = (SnakeDraw.dead_option_number -
