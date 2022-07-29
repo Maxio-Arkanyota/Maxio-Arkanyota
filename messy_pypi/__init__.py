699da22599ced5e5909a8c620464d2b8ac842856
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-07-29 00:59:48 +0000

    day : 210, commit : 105/210 -> `\t`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -742,3 +742,4 @@ class SnakeActions:
 			SnakeDraw.dead_option_number = (SnakeDraw.dead_option_number + 1)
 		SnakeDraw.dead_option_number %= 2  # CAR y a 2 options Quit, Restart
 		SnakeDraw.draw_dead_options()
+	
