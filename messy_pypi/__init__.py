10b5b001f7de5dbc598e1de570107a73629a9edd
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-07-30 01:33:26 +0000

    day : 211, commit : 169/211 -> `t`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -750,4 +750,4 @@ class SnakeActions:
 			"z": cls.change_option_dead,
 			"\x1b[A": cls.change_option_dead,
 			"s": cls.change_option_dead,
-			"\x1b[B": cls.change_op
+			"\x1b[B": cls.change_opt
