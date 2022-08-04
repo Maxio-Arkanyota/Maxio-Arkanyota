28d4b3b87c892e3d31c0d9d0daaf6a09e5f716f3
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-08-04 01:14:00 +0000

    day : 216, commit : 132/216 -> `e`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -790,4 +790,4 @@ class SnakeActions:
 		if not SnakeDraw.lock:
 			if directions[kwargs["clean_key"]] % 2 != SnakeDraw.facing % 2:
 				SnakeDraw.facing = directions[kwargs["clean_key"]]
-				Snak
+				Snake
