4525bbb185644f09d803ba9236af9cd0b2a18380
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-08-06 00:19:14 +0000

    day : 218, commit : 028/218 -> `p`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -797,4 +797,4 @@ class SnakeActions:
 			SnakeDraw.option_number = (SnakeDraw.option_number - 1)
 		if kwargs["clean_key"] in ["s", "\x1b[B"]:
 			SnakeDraw.option_number = (SnakeDraw.option_number + 1)
-		SnakeDraw.option_number %= len(SnakeDraw.menu_o
+		SnakeDraw.option_number %= len(SnakeDraw.menu_op
