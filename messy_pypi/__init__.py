ebae71a07062774c213ee0e9c562a309df708531
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-08-06 01:24:30 +0000

    day : 218, commit : 152/218 -> `n`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -801,4 +801,4 @@ class SnakeActions:
 		SnakeDraw.draw_options()
 	@classmethod
 	def do_option_action(cls, **kwargs):
-		option = tuple(SnakeDraw.menu_optio
+		option = tuple(SnakeDraw.menu_option
