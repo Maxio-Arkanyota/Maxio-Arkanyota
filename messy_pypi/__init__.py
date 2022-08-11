0df15be0cb2f3c2bbe3550a572a2532b51f93849
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-08-11 00:33:07 +0000

    day : 223, commit : 054/223 -> `o`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -833,4 +833,4 @@ def show_shortcuts():  # TODO ou a remove
 def quit_menu():
 	SnakeDraw.show_menu()
 class SnakeDraw:
-	lock = False  # Pour éviter le double acti
+	lock = False  # Pour éviter le double actio
