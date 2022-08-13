625cc741fe5b4a89984a20b4e01889bc462163d7
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-08-13 01:35:05 +0000

    day : 225, commit : 172/225 -> `f`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -854,4 +854,4 @@ class SnakeDraw:
 	def restart(cls, **kwargs):
 		cls.menu = False
 		cls.snake_pos = [(cls.size // 2, cls.size // 2)]
-		cls.facing = 0  # 0 right, 1: up, 2: le
+		cls.facing = 0  # 0 right, 1: up, 2: lef
