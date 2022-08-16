b6afb328bebd38ca7c67175a4d2bae941a76a363
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-08-16 01:54:55 +0000

    day : 228, commit : 209/228 -> `c`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -879,4 +879,4 @@ class SnakeDraw:
 			cls.option_number = 0
 			cls.draw_options()
 	menu_options = {
-		# OPTION: [Curent_option(Default_option), [sele
+		# OPTION: [Curent_option(Default_option), [selec
