9707da6b66ee5013c9f5f2881df3dcbd1339d037
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-08-18 00:38:18 +0000

    day : 230, commit : 064/230 -> `e`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -886,4 +886,4 @@ class SnakeDraw:
 		"Themes": [0, ["Normal", "Full", "Custom", "WindowsCompatibility"]],
 		# "Show Shortcut": show_shortcuts,
 		"Continue": quit_menu,
-		"Restart": gam
+		"Restart": game
