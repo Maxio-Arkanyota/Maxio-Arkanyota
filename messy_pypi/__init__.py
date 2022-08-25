28a6a33eee522b0e8df55eb0ae0ffd193aea32cb
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-08-25 02:09:22 +0000

    day : 237, commit : 236/237 -> `o`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -937,4 +937,4 @@ class SnakeDraw:
 			"█  █ █  █ █   █ █    █  █  █ █  █   █ █  ",
 			"████ █  █ █   █ ███  ████   █   ███ █  █ ",
 		)
-		for j in range(len(cls.log
+		for j in range(len(cls.logo
