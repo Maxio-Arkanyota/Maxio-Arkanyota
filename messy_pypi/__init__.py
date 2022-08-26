851401127110215f83deeff5b46a7991b7315103
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-08-26 00:33:08 +0000

    day : 238, commit : 054/238 -> `)`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -938,4 +938,4 @@ class SnakeDraw:
 			"████ █  █ █   █ ███  ████   █   ███ █  █ ",
 		)
 		for j in range(len(cls.logo_dead)):
-			print(f"\033[{j + 5};5H{cls.logo_dead[j]}"
+			print(f"\033[{j + 5};5H{cls.logo_dead[j]}")
