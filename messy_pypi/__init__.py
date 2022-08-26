b24c07c7090bf3ec0d45ccc31870bfd83c965d11
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-08-26 02:02:37 +0000

    day : 238, commit : 223/238 -> `}`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -943,4 +943,4 @@ class SnakeDraw:
 			# Draw Gameover
 			# : ← {func[1][func[0]]}
 			if cls.dead_option_number == i:
-				message = f"\033[33m\033[{i * 2 + 11};8H{cls.dead_options[i]
+				message = f"\033[33m\033[{i * 2 + 11};8H{cls.dead_options[i]}
