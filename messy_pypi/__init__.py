3cbf2fdae846e8e5e05e7785b5b3cb60c0834c5b
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-08-31 00:23:33 +0000

    day : 243, commit : 036/243 -> `t`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -977,4 +977,4 @@ class SnakeDraw:
 			if cls.menu or cls.dead:
 				pass
 			else:
-				# SET CODE HERE: ne pas metre de code bloquant: code qui nécessite une action de l'u
+				# SET CODE HERE: ne pas metre de code bloquant: code qui nécessite une action de l'ut
