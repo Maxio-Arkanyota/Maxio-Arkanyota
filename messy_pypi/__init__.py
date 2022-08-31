24bb90e1df7d21b6bd9592d11f4393ff63434529
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-08-31 01:31:16 +0000

    day : 243, commit : 164/243 -> `m`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -979,4 +979,4 @@ class SnakeDraw:
 			else:
 				# SET CODE HERE: ne pas metre de code bloquant: code qui nécessite une action de l'utilisateur
 				# Affiche la tête du snake
-				print(f"\033[{cls.snake_pos[-1][1]};{cls.snake_pos[-1][0]}H{Themes[cls.current_the
+				print(f"\033[{cls.snake_pos[-1][1]};{cls.snake_pos[-1][0]}H{Themes[cls.current_them
