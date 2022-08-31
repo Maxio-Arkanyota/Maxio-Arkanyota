212415c05bd1221e48569e5f5b2e9e58debf54e4
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-08-31 00:11:55 +0000

    day : 243, commit : 014/243 -> `s`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -977,4 +977,4 @@ class SnakeDraw:
 			if cls.menu or cls.dead:
 				pass
 			else:
-				# SET CODE HERE: ne pas metre de code bloquant: code qui néces
+				# SET CODE HERE: ne pas metre de code bloquant: code qui nécess
