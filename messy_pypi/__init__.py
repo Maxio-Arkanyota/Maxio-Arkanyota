b9de59d0c6578dc089e269ae251560282a14bc39
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-09-13 00:08:12 +0000

    day : 256, commit : 007/256 -> `3`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1084,4 +1084,4 @@ class SnakeKey:
 				if input_key == "\033":
 					with Nonblocking(sys.stdin):
 						input_key += sys.stdin.read(20)
-						if input_key.startswith("\0
+						if input_key.startswith("\03
