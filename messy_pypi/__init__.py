3cfed6a8bb6579a51be2b90a59dbc546e1db8093
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-09-14 01:09:01 +0000

    day : 257, commit : 122/257 -> `v`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1096,4 +1096,4 @@ class SnakeKey:
 				input_save = input_key
 				input_key = ""
 			if clean_key in SnakeActions.dico_actions.keys():
-				SnakeActions.dico_actions[clean_key](clean_key=clean_key, input_sa
+				SnakeActions.dico_actions[clean_key](clean_key=clean_key, input_sav
