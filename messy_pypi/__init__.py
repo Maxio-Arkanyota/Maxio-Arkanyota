3bb2b2bf9a91aad5ab8ea4ff6ea16999ca76170e
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-09-25 01:20:30 +0000

    day : 268, commit : 144/268 -> `p`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1244,4 +1244,4 @@ class tetris:
 		return grid
 	def convert_shape_format(self, shape):
 		positions = []
-		format = shape.shape[shape.rotation % len(sha
+		format = shape.shape[shape.rotation % len(shap
