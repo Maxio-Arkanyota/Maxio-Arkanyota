7761cd253cd55f232bc2a261e4662053d9d2ad2e
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-09-28 01:33:23 +0000

    day : 271, commit : 164/271 -> `_`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1269,4 +1269,4 @@ class tetris:
 				return True
 		return False
 	def get_shape(self):
-		return Piece(5, 0, random.choice(self.shapes), self.shapes, self.shape
+		return Piece(5, 0, random.choice(self.shapes), self.shapes, self.shape_
