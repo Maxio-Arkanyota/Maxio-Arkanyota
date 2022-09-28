94192b686fafa9bfa81aa679ed377d2bd92fbb1b
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-09-28 02:09:54 +0000

    day : 271, commit : 233/271 -> `o`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1271,4 +1271,4 @@ class tetris:
 	def get_shape(self):
 		return Piece(5, 0, random.choice(self.shapes), self.shapes, self.shape_colors)
 	def draw_text_middle(self, surface, text, size, color):
-		f
+		fo
