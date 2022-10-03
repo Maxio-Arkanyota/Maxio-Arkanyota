19a932b8df7407317708eb1aa368645ceb9cb488
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-10-03 01:59:11 +0000

    day : 276, commit : 217/276 -> `-`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1308,4 +1308,4 @@ class tetris:
 		font = pygame.font.SysFont('comicsans', 30)
 		label = font.render('Next Shape', 1, (255, 255, 255))
 		sx = self.top_left_x + self.play_width + 50
-		sy = self.top_left_y + self.play_height / 2 
+		sy = self.top_left_y + self.play_height / 2 -
