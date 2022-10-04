2684bba6183ff76eb8fd199b10cf27ad55dfacda
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-10-04 01:37:36 +0000

    day : 277, commit : 176/277 -> `f`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1314,4 +1314,4 @@ class tetris:
 			row = list(line)
 			for j, column in enumerate(row):
 				if column == '0':
-					pygame.draw.rect(surface, shape.color, (sx + j * sel
+					pygame.draw.rect(surface, shape.color, (sx + j * self
