4be9e4c926aaa50505c1eab2446a53fb31afe575
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-10-10 01:28:02 +0000

    day : 283, commit : 158/283 -> `f`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1354,4 +1354,4 @@ class tetris:
 								 (self.top_left_x + j * self.block_size, self.top_left_y + i * self.block_size,
 								  self.block_size, self.block_size), 0)
 		pygame.draw.rect(surface, (255, 0, 0), (self.top_left_x, self.top_left_y, self.play_width, self.play_height), 5)
-		sel
+		self
