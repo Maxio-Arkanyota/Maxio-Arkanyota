299390bab3d40aaf31435a430cd1606bdc5370e3
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-10-14 01:53:29 +0000

    day : 287, commit : 206/287 -> `1`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1394,4 +1394,4 @@ class tetris:
 						if not (self.valid_space(current_piece, grid)):
 							current_piece.x += 1
 					if event.key == pygame.K_RIGHT:
-						current_piece.x += 
+						current_piece.x += 1
