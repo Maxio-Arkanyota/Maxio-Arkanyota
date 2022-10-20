5163854a364faa5f1b2d56c3d4604e520938adfc
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-10-20 00:38:22 +0000

    day : 293, commit : 064/293 -> `:`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1440,4 +1440,4 @@ class tetris:
 					self.main()
 		pygame.display.quit()
 class Piece(object):  # *
-	def __init__(self, x, y, shape, shapes, shape_colors)
+	def __init__(self, x, y, shape, shapes, shape_colors):
