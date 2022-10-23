6047614560df38e9c2e50126c5a1e49c7ffb77e1
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-10-23 00:49:27 +0000

    day : 296, commit : 085/296 -> `p`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1474,4 +1474,4 @@ def check_for_duplicates(paths, hash=hashlib.sha1, remove=False):
 	for path in paths:
 		for dirpath, dirnames, filenames in os.walk(path):
 			for filename in filenames:
-				full_
+				full_p
