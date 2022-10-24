962dc11b445f4604f23f656452be0e5c1df5166b
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-10-24 02:33:08 +0000

    day : 297, commit : 281/297 -> `n`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1489,4 +1489,4 @@ def check_for_duplicates(paths, hash=hashlib.sha1, remove=False):
 				small_hash = get_hash(filename, first_chunk_only=True)
 				hashes_on_1k[(small_hash, size_in_bytes)].append(filename)
 			except (OSError,):
-				co
+				con
