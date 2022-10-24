8ecbb983ae75d9ffc09f4cadf4c18b0c76e83c4e
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-10-24 01:03:44 +0000

    day : 297, commit : 112/297 -> ` `

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1484,4 +1484,4 @@ def check_for_duplicates(paths, hash=hashlib.sha1, remove=False):
 	for size_in_bytes, files in hashes_by_size.items():
 		if len(files) < 2:
 			continue
-		for filename
+		for filename 
