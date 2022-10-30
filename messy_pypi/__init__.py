daeafc1d5293f7545901d2d584230e26c8dc177d
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-10-30 00:39:55 +0000

    day : 303, commit : 067/303 -> `l`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1531,4 +1531,4 @@ def readfile(file):
 		elif current_ligne[0:3] == "###":
 			current_ligne = "\033[32m\033[1m" + (' ' + current_ligne[3:] + " ").center(os.get_terminal_size()[0], ".") + "\033[0m"
 		elif current_ligne[0:2] == "##":
-			current_
+			current_l
