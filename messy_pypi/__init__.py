d1f02c8017fccba5ec5e778eda3fc861accea6c0
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-10-31 02:29:26 +0000

    day : 304, commit : 274/304 -> `[`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1538,4 +1538,4 @@ def readfile(file):
 		if "**" in current_ligne and not etat[0]:
 			etat[0] = True
 			current_ligne = current_ligne.replace("**", "\033[1m\033[91m", 1)
-		if "**" in current_ligne and etat
+		if "**" in current_ligne and etat[
