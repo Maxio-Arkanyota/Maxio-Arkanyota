eebcafe69b45a1ea04804708164a84efaa26df67
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-11-03 01:51:23 +0000

    day : 307, commit : 202/307 -> `"`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1562,4 +1562,4 @@ def LauchPrintMd():
 	if sys.argv[1:]:
 		printontermnal(readfile(sys.argv[1]))
 	else:
-		printontermnal(readfile(input("Ficher Markdown: 
+		printontermnal(readfile(input("Ficher Markdown: "
