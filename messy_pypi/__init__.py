18f30254ba2c353ad7c10e8d3118f869eb8323c8
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-11-04 01:42:57 +0000

    day : 308, commit : 186/308 -> `r`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1573,4 +1573,4 @@ def count_number_of_lines_in_file(file: str) -> int:
 	with open(file, "r", encoding="latin-1") as f:
 		text = f.readlines()
 		text = [e for e in text if e.strip() not in {""}]
-		retu
+		retur
