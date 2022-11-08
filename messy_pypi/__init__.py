44a71eab641ed04c14cb500e9a6105850d55b5b7
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-11-08 01:37:38 +0000

    day : 312, commit : 176/312 -> `:`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1603,4 +1603,4 @@ def count_number_of_lines_in_folder_verbose(folder: str, match: str = "(.py$|.md
 				print("\033[34mpath:\033[0m " + str(os.path.join(root, name)))
 				ligne = count_number_of_lines_in_file(os.path.join(root, name))
 				print("\033[35mlignes:\033[0m " + str(ligne))
-				if otherinfo
+				if otherinfo:
