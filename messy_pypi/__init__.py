098419d551af3a5d3af4fbc531ab92534850e7e3
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-11-10 01:11:11 +0000

    day : 314, commit : 126/314 -> `\t`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1623,3 +1623,4 @@ def count_number_of_lines_in_folder_verbose(folder: str, match: str = "(.py$|.md
 		for i in dico_otherinfo.keys():
 			max_len = max(len(i), max_len)
 		for i, j in dico_otherinfo.items():
+	
