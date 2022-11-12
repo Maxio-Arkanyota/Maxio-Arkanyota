58852fb4a0977ae65680bd8b9d924317bb7e38de
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-11-12 00:17:44 +0000

    day : 316, commit : 025/316 -> `"`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1641,4 +1641,4 @@ def count_number_of_lines_in_folder_verbose(folder: str, match: str = "(.py$|.md
 		try:
 			print("\n" + "=" * (os.get_terminal_size()[0]) + "\n")
 		except:
-			print("\n" + "=
+			print("\n" + "="
