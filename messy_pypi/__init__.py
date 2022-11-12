3f68fc6d83c820677d3fc819b6b3dd604d0c4e1f
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-11-12 02:28:57 +0000

    day : 316, commit : 273/316 -> `r`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1650,4 +1650,4 @@ def LaunchCountLines():
 		match = sys.argv[1]
 	else:
 		match = "\.py$|\.md$|\.html$|\.css$|\.txt$|LICENCE$|\.cfg$|\.json$"
-	print("nombre de lignes: " + st
+	print("nombre de lignes: " + str
