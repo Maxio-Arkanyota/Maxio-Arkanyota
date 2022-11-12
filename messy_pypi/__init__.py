123d613dc2e8ca3d04ae78f10f3e55e3ee9a5526
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-11-12 01:24:23 +0000

    day : 316, commit : 151/316 -> ` `

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1647,4 +1647,4 @@ def count_number_of_lines_in_folder_verbose(folder: str, match: str = "(.py$|.md
 		return nombres_lignes
 def LaunchCountLines():
 	if len(sys.argv) > 1:
-		match =
+		match = 
