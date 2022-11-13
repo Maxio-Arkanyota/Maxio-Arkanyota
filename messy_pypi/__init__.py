a62e9bcd119c136e08ec24067f046f49517b3494
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-11-13 00:34:07 +0000

    day : 317, commit : 056/317 -> `p`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1652,4 +1652,4 @@ def LaunchCountLines():
 		match = "\.py$|\.md$|\.html$|\.css$|\.txt$|LICENCE$|\.cfg$|\.json$"
 	print("nombre de lignes: " + str(count_number_of_lines_in_folder_verbose(".", match, otherinfo=True)))
 	print("match: " + match)
-	
+	p
