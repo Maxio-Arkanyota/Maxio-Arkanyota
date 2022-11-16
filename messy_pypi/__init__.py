7ff28d174d6813ae4e7274141b1a0b21941fa252
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-11-16 01:07:29 +0000

    day : 320, commit : 119/320 -> `l`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1684,4 +1684,4 @@ class ShellDraw:
 			f"\033[{ShellInfos.cursor_pos // os.get_terminal_size()[0] + 1};{ShellInfos.cursor_pos % os.get_terminal_size()[0]}H")
 	@classmethod
 	def actulise_input(cls):
-		sys.stdout.write("\033[0;0H" + She
+		sys.stdout.write("\033[0;0H" + Shel
