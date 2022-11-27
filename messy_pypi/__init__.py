4fae510a3ee40bb92054eca41d5ec4c0a8633d92
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-11-27 00:32:32 +0000

    day : 331, commit : 053/331 -> `l`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1801,4 +1801,4 @@ class ShellModes:
 			ShellDraw.clear_input()
 		elif ShellInfos.stack_key[-1:] == " ":
 			if ShellInfos.stack_key[:-1].isdigit():
-				ShellInfos.cursor_pos += int(Shel
+				ShellInfos.cursor_pos += int(Shell
