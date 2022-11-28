310b5c1c11e179bfbf277139a7a3543bb1af68be
Maxio-Arkanyota <arkanyota@hotmail.com>
2022-11-28 00:14:01 +0000

    day : 332, commit : 018/332 -> `i`

--- messy_pypi/__init__.py
+++ messy_pypi/__init__.py
@@ -1808,4 +1808,4 @@ class ShellModes:
 			ShellInfos.stack_key = ""
 		elif ShellInfos.stack_key[-1:] == "\x7f":
 			if ShellInfos.stack_key[:-1].isdigit():
-				ShellInfos.cursor_pos -= 
+				ShellInfos.cursor_pos -= i
